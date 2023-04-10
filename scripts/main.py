b81347469b83f3124cc9b9fb5a884163c7e9f6c4
todhm <todhm@nate.com>
2023-04-11 02:58:15 +0900

    change directory

--- scripts/main.py
+++ scripts/main.py
@@ -9,5 +9,10 @@ script_callbacks.on_ui_tabs(feed.on_ui_tabs)
 
 
 is_exists = os.path.exists('/content/repository/extensions-builtin')
-if is_exists:
-    shutil.copytree('extensions/sd_feed/javascript', '/content/repository/extensions-builtin/prompt-bracket-checker/javascript', dirs_exist_ok=True)
+is_exists_seconds = os.path.exists('/content/drive/MyDrive/SD/extensions/sd_feed/javascript')
+if is_exists and is_exists_seconds:
+    shutil.copytree(
+        '/content/drive/MyDrive/SD/extensions/sd_feed/javascript', 
+        '/content/repository/extensions-builtin/prompt-bracket-checker/javascript', 
+        dirs_exist_ok=True
+    )

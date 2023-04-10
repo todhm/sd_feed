4e365c434d291909259ca986bc4a6891ec0b1a51
todhm <todhm@nate.com>
2023-04-11 03:39:33 +0900

    fix style error

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+import os
 from modules import script_callbacks
 from newtype_v3.tabs import feed
 from newtype_v3.locs import is_exists, is_exists_seconds
@@ -13,3 +14,8 @@ if is_exists and is_exists_seconds:
         '/content/repository/extensions-builtin/prompt-bracket-checker/javascript', 
         dirs_exist_ok=True
     )
+    if not os.path.exists('/content/repository/extensions-builtin/LDSR/style.css'):
+        shutil.copy(
+            '/content/drive/MyDrive/SD/extensions/sd_feed/style.css', 
+            '/content/repository/extensions-builtin/LDSR/style.css'
+        )

7f5b099b7b155fd318c68a9e0953da3858ebcc87
todhm <todhm@nate.com>
2023-04-14 13:23:44 +0900

    new user json

--- newtype_v3/users.py
+++ newtype_v3/users.py
@@ -8,6 +8,20 @@ def create_user() -> Dict:
     try:
         with open(f'{DEFAULT_LOC}/user.json') as f:
             user_dict = json.load(f)
+            user_id = user_dict.get('userId')
+            if not user_dict.get("token"):
+                post_dict = {
+                    'userId': user_id,
+                    'useAuth': True,
+                }
+                response = requests.post(
+                    'https://newtypev3-server-vjiloyvjvq-an.a.run.app/user/user', 
+                    json=post_dict
+                )
+                token = response.json().get('token')
+                user_dict['token'] = token
+                with open(f'{DEFAULT_LOC}/user.json', 'w') as f:
+                    json.dump(user_dict, f, sort_keys=True, indent=4)
             return user_dict
     except Exception:
         userId = str(uuid.uuid4())
@@ -20,6 +34,7 @@ def create_user() -> Dict:
             json=user_dict
         )
         token = response.json().get('token')
+        print(token)
         user_dict['token'] = token
         with open(f'{DEFAULT_LOC}/user.json', 'w') as f:
             json.dump(user_dict, f, sort_keys=True, indent=4)
